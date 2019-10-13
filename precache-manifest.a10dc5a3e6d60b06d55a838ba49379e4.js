cc35022edf62f7e71cafa740197a8f3c0c6e6d12
bmd-rizqireza <rizqi.valhevi@bhinneka.com>
2019-10-13 23:07:27 +0700

    Updates

---
+++ precache-manifest.a10dc5a3e6d60b06d55a838ba49379e4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c38b9ea28d9b444c445d",
+    "url": "/e-kavling/static/css/main.e7b2c9a4.chunk.css"
+  },
+  {
+    "revision": "c38b9ea28d9b444c445d",
+    "url": "/e-kavling/static/js/main.c38b9ea2.chunk.js"
+  },
+  {
+    "revision": "25d0362e30698521395b",
+    "url": "/e-kavling/static/css/1.6080cc84.chunk.css"
+  },
+  {
+    "revision": "25d0362e30698521395b",
+    "url": "/e-kavling/static/js/1.25d0362e.chunk.js"
+  },
+  {
+    "revision": "b1e2e688a18fcc10f4d5",
+    "url": "/e-kavling/static/js/runtime~main.b1e2e688.js"
+  },
+  {
+    "revision": "6187472a01bc463bd34aa87e05bd71b3",
+    "url": "/e-kavling/static/media/logo.6187472a.png"
+  },
+  {
+    "revision": "4e05a093d69baf3da2ba46572b0d8e2c",
+    "url": "/e-kavling/static/media/bg-durian.4e05a093.jpg"
+  },
+  {
+    "revision": "71bd4a0cad46e961f80f07dd320a92bf",
+    "url": "/e-kavling/static/media/not-found.71bd4a0c.svg"
+  },
+  {
+    "revision": "8610d7453b99a9ebf836a1d7b021c140",
+    "url": "/e-kavling/static/media/error.8610d745.svg"
+  },
+  {
+    "revision": "ee78e4c272c5170d247ad5fb625d0c4a",
+    "url": "/e-kavling/index.html"
+  }
+];
